c7b9e3367344744f797c090fbec17bf0dc053584
Richard Yance <yance0022@gmail.com>
2018-10-18 14:27:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ end
     array.push(name)
     position = array.index(name)
       puts "Welcome, #{name}. You are number #{array.index(name)+ 1} in line."
-    return name, position
+    return array
   end
  def now_serving(array)
   if array.empty? == true
