e51b94ccf569b494c31ecd669f13b64876d47f4d
Richard Yance <yance0022@gmail.com>
2018-10-18 14:26:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(array)
     puts "The line is currently empty."
   end
 end
- def take_a_number(array, name) 
+ def take_a_number(array, name)
     array.push(name)
     position = array.index(name)
       puts "Welcome, #{name}. You are number #{array.index(name)+ 1} in line."
